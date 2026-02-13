2948d9b70e722a00c15c7646af467a19c07948bd
kimsg125 <kimsg125@korea.ac.kr>
2026-02-13 17:25:15 +0900

    commit

--- boj_java/VSCode/b18185.java
+++ boj_java/VSCode/b18185.java
@@ -3,9 +3,10 @@ import java.util.*;
 
 class b18185 {
 
-    static int N, sum;
+    static int N, sumF, sumB;
 
-    static int[] ramen;
+    static int[] ramenF;
+    static int[] ramenB;
 
     public static void main(String[] args) throws Exception {
 
@@ -13,33 +14,52 @@ class b18185 {
 
         N = Integer.parseInt(br.readLine());
 
-        ramen = new int[N];
+        ramenF = new int[N];
+        ramenB = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = N - 1; i >= 0; i--) {
-            ramen[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            ramenF[i] = Integer.parseInt(st.nextToken());
+            ramenB[N - 1 - i] = ramenF[i];
         }
 
         for (int i = 0; i < N; i++) {
-            while (ramen[i] > 0) {
-                if (i < N - 2 && ramen[i + 1] > 0 && ramen[i + 2] > 0) {
-                    int minNum = Math.min(ramen[i], Math.min(ramen[i + 1], ramen[i + 2]));
-                    sum += 7 * minNum;
-                    ramen[i] -= minNum;
-                    ramen[i + 1] -= minNum;
-                    ramen[i + 2] -= minNum;
-                } else if (i < N - 1 && ramen[i] > 0 && ramen[i + 1] > 0) {
-                    int minNum = Math.min(ramen[i], ramen[i + 1]);
-                    sum += 5 * minNum;
-                    ramen[i] -= minNum;
-                    ramen[i + 1] -= minNum;
-                } else if (ramen[i] > 0) {
-                    sum += 3 * ramen[i];
-                    ramen[i] = 0;
+            while (ramenF[i] > 0) {
+                if (i < N - 2 && ramenF[i + 1] > 0 && ramenF[i + 2] > 0) {
+                    int minNum = Math.min(ramenF[i], Math.min(ramenF[i + 1], ramenF[i + 2]));
+                    sumF += 7 * minNum;
+                    ramenF[i] -= minNum;
+                    ramenF[i + 1] -= minNum;
+                    ramenF[i + 2] -= minNum;
+                } else if (i < N - 1 && ramenF[i] > 0 && ramenF[i + 1] > 0) {
+                    int minNum = Math.min(ramenF[i], ramenF[i + 1]);
+                    sumF += 5 * minNum;
+                    ramenF[i] -= minNum;
+                    ramenF[i + 1] -= minNum;
+                } else if (ramenF[i] > 0) {
+                    sumF += 3 * ramenF[i];
+                    ramenF[i] = 0;
+                }
+            }
+            while (ramenB[i] > 0) {
+                if (i < N - 2 && ramenB[i + 1] > 0 && ramenB[i + 2] > 0) {
+                    int minNum = Math.min(ramenB[i], Math.min(ramenB[i + 1], ramenB[i + 2]));
+                    sumB += 7 * minNum;
+                    ramenB[i] -= minNum;
+                    ramenB[i + 1] -= minNum;
+                    ramenB[i + 2] -= minNum;
+                } else if (i < N - 1 && ramenB[i] > 0 && ramenB[i + 1] > 0) {
+                    int minNum = Math.min(ramenB[i], ramenB[i + 1]);
+                    sumB += 5 * minNum;
+                    ramenB[i] -= minNum;
+                    ramenB[i + 1] -= minNum;
+                } else if (ramenB[i] > 0) {
+                    sumB += 3 * ramenB[i];
+                    ramenB[i] = 0;
                 }
             }
         }
 
-        System.out.println(sum);
+        System.out.println(Math.min(sumF, sumB));
 
     }
 
